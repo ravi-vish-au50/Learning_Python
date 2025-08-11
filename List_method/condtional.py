bdc42a0f860d3df06fe603fe6c27785a67ddc65b
ravi-vish-au50 <ravivish166@gmail.com>
2025-08-11 21:12:13 +0530

    done

---
+++ List_method/condtional.py
@@ -0,0 +1,38 @@
+                                            #  Python Control Statment
+                                #              O
+                                #              |
+                                #              |
+                                # ----------------------  
+                                # |                     |
+                                # |    Condiotions      |---------------------
+                                # |                     |                     |
+                                # ----------------------                      |
+                                                                            
+
+
+a ,b = 56 , 76
+if b>a : 
+    print("less than")
+
+
+    aman, vinod = 100 , 107
+    if vinod > aman :
+        print("vinod has scored more than aman")
+    elif aman == vinod:
+        print("both score equal")
+    else :
+        print('aman grater than vinod')    
+
+
+marks = 33
+if marks > 80 :
+    print("a+ grade")
+elif marks >70 :
+    print("A Grade")
+elif marks >60 :
+    print("Pass first divion")
+elif marks >= 33 :
+    print("pass second divion")
+else:
+    print("fail")
+    # sdjfgskdfbks
