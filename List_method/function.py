0dc01f0bcf93b10075cf1dda823279925cbd3ea6
ravi-vish-au50 <ravivish166@gmail.com>
2025-08-19 20:35:26 +0530

    python function day one

---
+++ List_method/function.py
@@ -0,0 +1,52 @@
+
+
+
+def Dosomething():
+    print("thi is my first function") #difinig the fuction
+Dosomething()            
+
+print('hello world')
+
+name = "Shardha"
+age = 23
+price = 25.99
+
+print(name , age , price)'
+
+a , b =1, '3'
+c = int(b)
+sum=a+c
+print(sum)
+
+Input from user
+
+name = input('Enter your name:')
+
+print("Welcom", name)
+
+num = int(input("enter your lucky number:"))
+print("your lucky number is ", num)
+
+write a program input 2 number and print their sum
+
+
+
+first = int(input("enter first:"))
+second = int(input("enter your second:"))
+print("sum:",first + second)
+
+
+side = float(input("enter first side:"))
+print("square:",side*side)
+
+WAP input 2 floting point numbers & print their avarrage
+
+first = float(input("enter first num:"))
+second = float(input("enter second num:"))
+
+print("avarrage:", (first+second)/2)
+
+first = int(input("enter first num:"))
+second = int(input("enter second num:"))
+
+print(first >= second)
