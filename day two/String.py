4f664271fe9a4d49eb6278fd73726093641618bf
ravi-vish-au50 <ravivish166@gmail.com>
2025-08-20 20:47:56 +0530

    new

---
+++ day two/String.py
@@ -0,0 +1,118 @@
+# str1 = "new string"
+# str2 = 'new string'
+# str3 = """new string""""
+
+# Concatenate
+
+# str1 = "apna"
+# str2 =  " college"
+# final_str= str1+ str2
+# print(final_str) 
+
+# Indexing
+
+# str = "apna college"
+
+# ch = str[3]
+# print(ch)
+
+# # slicing
+# str = "apna college"
+# # print(str[5:12])
+# print(str[5:len(str)])
+
+# Function string
+
+# str = "i am studing python from Apnacollege"
+
+# print(str.endswith('ege'))
+
+# print(str.capitalize())
+
+# print(str.replace('o','a'))
+
+# print(str.find('y'))
+
+# print(str.count(''))
+
+# Lets practics
+
+# WAP input from user's first name and prints its lenths
+
+# name = input("firstname:")
+# print("firstname:", len(name))
+
+
+# str = "Hi $i am $raja babu of my $special $respected $sir"
+# print(str.count("$"))
+
+# Conditional Statement
+
+# age = 19
+# if (age>= 18):
+#     print("can vote and  apply a lic")
+# else :
+#     print("you are not elegible")
+
+# light = "red"
+# if(light == "green"):
+#     print("go to your destination")
+# elif (light == "yellow"):
+#     print ("ready to go your destinations")
+# elif (light == "blue"):
+#     print ("light is situatted wrong")
+# else :
+#     print ("stop and wait until signal will be green")
+
+# Student Grading System
+
+# grade = 58
+# if(grade >=80 ):
+#     print("grade:= A")
+# elif(grade >= 70):
+#     print("Grade:= B ")
+# elif(grade>=60):
+#     print("grade:=C")
+# else :
+#     print ("student not quailify")
+
+
+# Nesting conditional
+
+# age = 45
+
+# if(age >= 18):
+#     if(age >=80):
+#         print("cannot drive ")
+#     else :
+#         print("can drive")
+# else :
+#     print("cannot drive")
+
+# lets practics
+# WAP
+# num = int(input("enter a numer"))
+
+# if(num % 2 == 0):
+#     print("even")
+# else: 
+#     print("odd")
+
+
+# a = int(input("enter first numer"))
+# b = int(input("enter second numer"))
+# c  = int(input("enter third numer"))
+
+# if(a>b and a>c):
+#     print(a)
+# elif(b>c):
+#     print(b)
+# else:
+#     print(c)
+# x = int(input("enter numer"))
+
+# if(x % 7 == 0):
+#  print("multiple of 7 ")
+# else:
+#  print("not multiple of 7 ")
+# ddv
