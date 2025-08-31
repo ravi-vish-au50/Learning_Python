712dd9ed74e2503091dec9a531e744edfbf41fa9
ravi-vish-au50 <ravivish166@gmail.com>
2025-08-31 22:27:34 +0530

    new

--- new/Conditional.py
+++ new/Conditional.py
@@ -1,3 +1,4 @@
+
 # Conditional  module for handling conditional statements in a custom scripting language.
 # if & indentaion based blocks
 # a = int(input("Enter first number: ")
@@ -5,22 +6,82 @@
 #         print("Positive")
 #         print("End of if block")
         
-place = input("Enter your country: ")
-a = int(input("Enter your age: "))
-if place == "Denmark":
-    if a >= 18:
-        print("You can vote in Denmark.")
-    else:
-        print("You cannot vote in Denmark.")
-elif place == "USA":
-    if a >= 18:
-        print("You can vote in the USA.")
-    else:
-        print("You cannot vote in the USA.")
-else:
-    print("Voting rules unknown for your country.")
+# place = input("Enter your country: ")
+# a = int(input("Enter your age: "))
+# if place == "Denmark":
+#     if a >= 18:
+#         print("You can vote in Denmark.")
+#     else:
+#         print("You cannot vote in Denmark.")
+# elif place == "USA":
+#     if a >= 18:
+#         print("You can vote in the USA.")
+#     else:
+#         print("You cannot vote in the USA.")
+# else:
+#     print("Voting rules unknown for your country.")
 
 
-    # nested if statements
-    # if-elif-else ladder
+#     # nested if statements
+#     # if-elif-else ladder
+
+# place = input("Enter your country: ")
+# bestfreind = input("Enter your best friend's country: ")
+
+# if place == "Denmark":
+#     if bestfreind == "Denmark":
+#         print("You and your best friend can vote in Denmark.")
+#     else:
+#         print("You can vote in Denmark, but your best friend cannot.")
+
+# print(place)
+# #     # switch-case using dictionary mapping
+
+# num = int(input("Enter a number between 1 and 5: "))
+# if num < 1 or num > 5:
+#     print("Number out of range.")  
+# else:
+#     switcher = {
+#         1: "You entered one.",
+#         2: "You entered two.",
+#         3: "You entered three.",
+#         4: "You entered four.",
+#         5: "You entered five."
+#     }
+#     print(switcher.get(num))
+
+# place = input("Enter your place: ")
+# bestfreind = input("is my bestfriend going?!: ")
+# sponsor = input("is there any sponsor: ")
+# if place == "Hill Station":
+#   print("I may go !!!")
+# if bestfreind == "yes":
+#         print("lets go together")
+# elif sponsor == "yes":
+#         print("I will go alone")
+# else:
+#         print("I will not go")
+
+# Elif condition
     
+# a = int(input("Enter your number: "))
+# if a > 0:
+#     print("Positive")
+# elif a < 0:
+#     print("Negative")
+# elif a == 0:
+#     print("Zero")
+# else:
+#     print("Invalid input")
+
+# Assignment operators
+# height_A = int(input("Enter height of A: "))
+# height_B = int(input("Enter height of B: "))
+# height_C = int(input("Enter height of C: "))
+
+# if height_A > height_B and height_A > height_C:
+#     print("A is the tallest")
+# elif height_B > height_A and height_B > height_C:
+#     print("B is the tallest")  
+# else:
+#     print("C is the tallest")
